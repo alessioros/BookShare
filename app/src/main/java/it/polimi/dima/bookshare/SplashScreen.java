4b7ddc26f76541d647296e53de22240081b425f6
alessiorossotti <alessiorossotti@gmail.com>
2016-03-22 15:33:13 +0100

    graphic and login

--- app/src/main/java/it/polimi/dima/bookshare/SplashScreen.java
+++ app/src/main/java/it/polimi/dima/bookshare/SplashScreen.java
@@ -1,14 +1,14 @@
 package it.polimi.dima.bookshare;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import it.polimi.dima.bookshare.activities.MainActivity;
+import it.polimi.dima.bookshare.activities.LoginActivity;
 
-public class SplashScreen extends Activity {
+public class SplashScreen extends AppCompatActivity {
 
     // Splash screen timer
     private static int SPLASH_TIME_OUT = 2000;
@@ -33,12 +33,10 @@ public class SplashScreen extends Activity {
 
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+
+                Intent i = new Intent(SplashScreen.this, LoginActivity.class);
                 startActivity(i);
 
-                // close this activity
                 finish();
             }
         }, SPLASH_TIME_OUT);
---
+++ app/src/main/java/it/polimi/dima/bookshare/activities/LoginActivity.java
@@ -0,0 +1,49 @@
+package it.polimi.dima.bookshare.activities;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import it.polimi.dima.bookshare.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView title = (TextView) findViewById(R.id.logo);
+        EditText email = (EditText) findViewById(R.id.email);
+        EditText password = (EditText) findViewById(R.id.password);
+        TextView register = (TextView) findViewById(R.id.register);
+
+        Typeface zaguatica = Typeface.createFromAsset(getAssets(), "fonts/zaguatica-Bold.otf");
+        Typeface aller = Typeface.createFromAsset(getAssets(), "fonts/Aller_Rg.ttf");
+
+        title.setTypeface(zaguatica);
+        email.setTypeface(aller);
+        password.setTypeface(aller);
+        register.setTypeface(aller);
+
+        Button login = (Button) findViewById(R.id.login_button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                LoginActivity.this.finish();
+            }
+        });
+    }
+}
