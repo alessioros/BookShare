ce9d7681700e2f7da3c272fe93bf2e8a445b1855
alessiorossotti <alessiorossotti@gmail.com>
2016-04-09 18:28:16 +0200

    fixes

--- app/src/main/java/it/polimi/dima/bookshare/amazon/CognitoSyncClientManager.java
+++ app/src/main/java/it/polimi/dima/bookshare/amazon/CognitoSyncClientManager.java
@@ -11,18 +11,9 @@ import com.amazonaws.regions.Regions;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by matteo on 25/03/16.
- */
 public class CognitoSyncClientManager {
 
-    private static final String TAG = "CognitoSyncClientManager";
-
-    /**
-     * Enter here the Identity Pool associated with your app and the AWS
-     * region where it belongs. Get this information from the AWS console.
-     */
-
+    private static final String TAG = "CognitoSyncClientM";
     private static final String IDENTITY_POOL_ID = "eu-west-1:cbf27841-7455-4e20-91dc-1cd49cc55558";
     private static final Regions REGION = Regions.EU_WEST_1;
 
@@ -30,11 +21,6 @@ public class CognitoSyncClientManager {
     protected static CognitoCachingCredentialsProvider credentialsProvider = null;
 
 
-    /**
-     * Initializes the Cognito Identity and Sync clients. This must be called before getInstance().
-     *
-     * @param context a context of the app
-     */
     public static void init(Context context) {
 
         if (syncClient != null) return;
@@ -45,13 +31,6 @@ public class CognitoSyncClientManager {
         syncClient = new CognitoSyncManager(context, REGION, credentialsProvider);
     }
 
-    /**
-     * Sets the login so that you can use authorized identity. This requires a
-     * network request, so you should call it in a background thread.
-     *
-     * @param providerName the name of the external identity provider
-     * @param token openId token
-     */
     public static void addLogins(String providerName, String token) {
         if (syncClient == null) {
             throw new IllegalStateException("CognitoSyncClientManager not initialized yet");
@@ -65,12 +44,6 @@ public class CognitoSyncClientManager {
         credentialsProvider.setLogins(logins);
     }
 
-    /**
-     * Gets the singleton instance of the CognitoClient. init() must be called
-     * prior to this.
-     *
-     * @return an instance of CognitoClient
-     */
     public static CognitoSyncManager getInstance() {
         if (syncClient == null) {
             throw new IllegalStateException("CognitoSyncClientManager not initialized yet");
@@ -78,11 +51,6 @@ public class CognitoSyncClientManager {
         return syncClient;
     }
 
-    /**
-     * Returns a credentials provider object
-     *
-     * @return
-     */
     public static CognitoCredentialsProvider getCredentialsProvider() {
         return credentialsProvider;
     }
