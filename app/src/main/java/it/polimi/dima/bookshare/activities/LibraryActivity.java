b927760259cfb0ec879d490e859833e2058537cd
alessiorossotti <alessiorossotti@gmail.com>
2016-04-12 11:12:23 +0200

    graphic

--- app/src/main/java/it/polimi/dima/bookshare/activities/LibraryActivity.java
+++ app/src/main/java/it/polimi/dima/bookshare/activities/LibraryActivity.java
@@ -1,5 +1,6 @@
 package it.polimi.dima.bookshare.activities;
 
+import android.graphics.Typeface;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
