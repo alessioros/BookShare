c0540f94f41b31c5bd85400d313368ac977b7b4c
alessiorossotti <alessiorossotti@gmail.com>
2016-05-06 11:19:15 +0200

    user profile's link to reviews

--- app/src/main/java/it/polimi/dima/bookshare/activities/UserProfileActivity.java
+++ app/src/main/java/it/polimi/dima/bookshare/activities/UserProfileActivity.java
@@ -1,26 +1,29 @@
 package it.polimi.dima.bookshare.activities;
 
-import android.content.Intent;
+import android.app.Fragment;
 import android.graphics.Typeface;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.transition.Slide;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 
 import it.polimi.dima.bookshare.R;
+import it.polimi.dima.bookshare.amazon.DynamoDBManager;
+import it.polimi.dima.bookshare.fragments.ReviewsAboutMeFragment;
 import it.polimi.dima.bookshare.tables.Review;
 import it.polimi.dima.bookshare.tables.User;
 import it.polimi.dima.bookshare.utils.ManageUser;
@@ -29,6 +32,9 @@ public class UserProfileActivity extends AppCompatActivity {
 
     private User owner;
     private ArrayList<Review> userReviews;
+    private boolean reviewsFragmentLoaded = false;
+    private final String TAG = "review_tag";
+    private Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +55,7 @@ public class UserProfileActivity extends AppCompatActivity {
 
         if (extras != null) {
 
-            userReviews = extras.getParcelableArrayList("user_reviews");
+            userReviews = (ArrayList<Review>) extras.getParcelableArrayList("user_reviews").clone();
             owner = extras.getParcelable("user");
             float avgRating = extras.getFloat("avg_rating");
 
@@ -97,15 +103,53 @@ public class UserProfileActivity extends AppCompatActivity {
 
             if (userReviews.size() > 0) {
 
-                numReviews.setText(userReviews.size() + " " + getResources().getString(R.string.owner_reviews_count));
-                avgRatingText.setText("" + avgRating);
-                avgRatingCount.setRating(avgRating);
+                assert numReviews != null;
+                numReviews.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
 
-            } else if (userReviews.size() == 1) {
+                        new LoadReviewers(new OnReviewersLoadingCompleted() {
+                            @Override
+                            public void onReviewersLoadingCompleted(ArrayList<User> reviewers) {
 
-                numReviews.setText(userReviews.size() + " " + getResources().getString(R.string.owner_review_count));
-                avgRatingText.setText("" + avgRating);
-                avgRatingCount.setRating(avgRating);
+                                CoordinatorLayout cl = (CoordinatorLayout) findViewById(R.id.coordinator_layout);
+                                cl.setVisibility(View.GONE);
+
+                                Toolbar toolbar = (Toolbar) findViewById(R.id.owner_reviewlist_toolbar);
+                                toolbar.setTitle(owner.getName() + "'s Reviews");
+                                toolbar.setVisibility(View.VISIBLE);
+
+                                fragment = ReviewsAboutMeFragment.newInstance();
+                                Bundle args = new Bundle();
+                                args.putParcelableArrayList("reviews", userReviews);
+                                args.putParcelableArrayList("reviewers", reviewers);
+                                fragment.setArguments(args);
+
+                                reviewsFragmentLoaded = true;
+                                getFragmentManager().beginTransaction()
+                                        .replace(R.id.content_frame, fragment)
+                                        .addToBackStack(TAG)
+                                        .commit();
+                            }
+                        }).execute(userReviews);
+
+                    }
+                });
+
+                if (userReviews.size() > 1) {
+
+                    numReviews.setText(userReviews.size() + " " + getResources().getString(R.string.owner_reviews_count));
+
+                    avgRatingText.setText("" + avgRating);
+                    avgRatingCount.setRating(avgRating);
+
+                } else {
+
+                    numReviews.setText(userReviews.size() + " " + getResources().getString(R.string.owner_review_count));
+
+                    avgRatingText.setText("" + avgRating);
+                    avgRatingCount.setRating(avgRating);
+                }
 
             } else {
 
@@ -129,12 +173,55 @@ public class UserProfileActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onBackPressed() {
+
+        if (reviewsFragmentLoaded) {
+
+            getFragmentManager().beginTransaction().remove(fragment).commit();
+
+            CoordinatorLayout cl = (CoordinatorLayout) findViewById(R.id.coordinator_layout);
+            cl.setVisibility(View.VISIBLE);
+
+            Toolbar toolbar = (Toolbar) findViewById(R.id.owner_reviewlist_toolbar);
+            toolbar.setVisibility(View.GONE);
+
+            reviewsFragmentLoaded = false;
+
+        }
+    }
+
     private void initActivityTransitions() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Slide transition = new Slide();
             transition.excludeTarget(android.R.id.statusBarBackground, true);
             getWindow().setEnterTransition(transition);
             getWindow().setReturnTransition(transition);
+        }
+    }
+
+    private interface OnReviewersLoadingCompleted {
+        void onReviewersLoadingCompleted(ArrayList<User> reviewers);
+    }
+
+    class LoadReviewers extends AsyncTask<ArrayList<Review>, ArrayList<User>, ArrayList<User>> {
+        private OnReviewersLoadingCompleted listener;
+
+        public LoadReviewers(OnReviewersLoadingCompleted listener) {
+            this.listener = listener;
+        }
+
+        @Override
+        protected ArrayList<User> doInBackground(ArrayList<Review>... params) {
+
+            DynamoDBManager DDBM = new DynamoDBManager(UserProfileActivity.this);
+
+            return DDBM.getReviewers(params[0]);
+        }
+
+        protected void onPostExecute(ArrayList<User> reviewers) {
+
+            listener.onReviewersLoadingCompleted(reviewers);
         }
     }
 }
